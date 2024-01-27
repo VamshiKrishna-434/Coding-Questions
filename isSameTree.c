e6fa6d6b0eebd97827d48ecdc20236f223345529
VamshiKrishna-434 <115861228+VamshiKrishna-434@users.noreply.github.com>
2024-01-28 00:37:15 +0530

    Create isSameTree.c

---
+++ isSameTree.c
@@ -0,0 +1,24 @@
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Code::
+ #include <stdbool.h>
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+ 
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+    
+    if (p == NULL || q == NULL) {
+        return false;
+    }
+
+
+    if (p->val != q->val) {
+        return false;
+    }
+    
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
